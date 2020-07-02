4ec4aba381ce6f2972a7987ca7540de21c4af00a
Peter Rufo <peter@peterrufo.com>
2020-07-02 16:37:16 -0400

    Updates

---
+++ precache-manifest.83ddcc17861cdc11a2f9d0509de1f6dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0445f0b0f26a1e99dc21423768df23",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "af73fdcd8fa817d3bb1b",
+    "url": "/portfolio-react/static/css/main.a019c395.chunk.css"
+  },
+  {
+    "revision": "89a9654e213d487bba8e",
+    "url": "/portfolio-react/static/js/2.2957118d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.2957118d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af73fdcd8fa817d3bb1b",
+    "url": "/portfolio-react/static/js/main.660c5222.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "b3f3458828563b49d8771faa8106e489",
+    "url": "/portfolio-react/static/media/cc.b3f34588.png"
+  },
+  {
+    "revision": "6da1de16d4a9e8a927607e0f092df775",
+    "url": "/portfolio-react/static/media/data.6da1de16.png"
+  },
+  {
+    "revision": "d6873c34759f79b87d52f39dbd86b407",
+    "url": "/portfolio-react/static/media/email.d6873c34.png"
+  },
+  {
+    "revision": "1502f7051f45fdea44835edea33e3b88",
+    "url": "/portfolio-react/static/media/front.1502f705.png"
+  },
+  {
+    "revision": "d429b02838724d1bea8343e292077cb0",
+    "url": "/portfolio-react/static/media/full.d429b028.png"
+  },
+  {
+    "revision": "0e4a882c6e48dc9f9725ec45eb355a38",
+    "url": "/portfolio-react/static/media/github.0e4a882c.png"
+  },
+  {
+    "revision": "a832904b98d55923ed1a64e0d1d85c95",
+    "url": "/portfolio-react/static/media/homeschooled.a832904b.png"
+  },
+  {
+    "revision": "1481e7f149f59364c359da22b952cea6",
+    "url": "/portfolio-react/static/media/olivetwist.1481e7f1.png"
+  },
+  {
+    "revision": "5a3cc7d7aab12b654fb84903413718fe",
+    "url": "/portfolio-react/static/media/quiz.5a3cc7d7.png"
+  },
+  {
+    "revision": "b23b4da6116f8f058c2ed8be6f66e819",
+    "url": "/portfolio-react/static/media/sdaf.b23b4da6.png"
+  }
+]);
